2252e34cec251d2826ab73bde480709642607b25
victorfowl <66727231+CorvoHisui@users.noreply.github.com>
2023-10-23 18:28:13 +0200

    Character controller started
    was about to implement animations

--- SC_Game/Source/SC_Game/SC_Game.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "SC_Game.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SC_Game, "SC_Game" );
